93ce5bca7038dd0ffc051d3111555a9593cbd108
hamster <malianbo@163.com>
2016-12-06 21:44:43 +0800

    update : init db sql

--- docs/init_db.sql
+++ docs/init_db.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.12)
 # Database: neitui
-# Generation Time: 2016-12-05 14:28:12 +0000
+# Generation Time: 2016-12-06 13:44:06 +0000
 # ************************************************************
 
 
@@ -46,49 +46,6 @@ CREATE TABLE `n2_company` (
 
 
 
-# Dump of table n2_resume
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `n2_resume`;
-
-CREATE TABLE `n2_resume` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '简历 ID',
-  `creator` int(11) NOT NULL DEFAULT '0' COMMENT '创建人',
-  `member_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '简历归属人 ID',
-  `created` int(11) NOT NULL COMMENT '创建时间',
-  `updator` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改人 ID',
-  `updated` int(11) NOT NULL COMMENT '修改时间',
-  `summary` text COMMENT '求职意向的补充说明',
-  `intent_post` varchar(64) DEFAULT NULL COMMENT '意向职位',
-  `work_type` int(11) DEFAULT NULL COMMENT '工作性质',
-  `work_city` varchar(32) DEFAULT NULL COMMENT '意向工作城市',
-  `intent_pay_from` int(11) DEFAULT NULL COMMENT '起薪',
-  `intent_pay_to` int(11) DEFAULT NULL COMMENT '薪水上限',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='简历表';
-
-
-
-# Dump of table n2_resume_delivery
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `n2_resume_delivery`;
-
-CREATE TABLE `n2_resume_delivery` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `resume_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '简历 ID',
-  `member_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '投递人 ID',
-  `delivery_time` int(11) NOT NULL COMMENT '投递时间',
-  `delivery_company_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '投递到的公司 ID',
-  `delivery_job_id` int(11) NOT NULL DEFAULT '0' COMMENT '投递',
-  `deliver_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '投递状态，0-待投递，1-已投递',
-  `receiver_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '受理状态，0-待受理，1-已受理',
-  `process_status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '处理状态，0-待处理，1-同意，2-拒绝，3-待定',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='简历投递记录';
-
-
-
 # Dump of table n2_favorite
 # ------------------------------------------------------------
 
@@ -114,24 +71,24 @@ CREATE TABLE `n2_job` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '岗位 ID',
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '岗位名称',
   `company_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '岗位所属公司 ID',
-  `addr_province` varchar(32) DEFAULT '' COMMENT '岗位所在地：省',
-  `addr_city` varchar(32) DEFAULT '' COMMENT '岗位所在地：市',
+  `addr_province` varchar(64) DEFAULT '' COMMENT '岗位所在地：省',
+  `addr_city` varchar(64) DEFAULT '' COMMENT '岗位所在地：市',
   `addr_area` varchar(64) DEFAULT '' COMMENT '岗位所在地：地区',
   `address` varchar(255) DEFAULT '' COMMENT '工作地点',
-  `edu_level` tinyint(1) unsigned DEFAULT '0' COMMENT '学历要求，0-不限',
-  `work_type` tinyint(1) unsigned DEFAULT '0' COMMENT '工作性质，0-不限，1-兼职，2-实习，3-全职',
-  `exp_level` tinyint(1) unsigned DEFAULT '0' COMMENT '工作经验要求，0-不限',
+  `edu_level` varchar(32) DEFAULT '0' COMMENT '学历要求，0-不限',
+  `work_type` varchar(32) DEFAULT '0' COMMENT '工作性质，0-不限，1-兼职，2-实习，3-全职',
+  `exp_level` varchar(32) DEFAULT '0' COMMENT '工作经验要求，0-不限',
   `summary_short` varchar(255) DEFAULT '' COMMENT '一句话简介',
   `summary` text COMMENT '岗位描述',
   `creator` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建人ID',
   `created` int(11) NOT NULL COMMENT '创建时间',
   `updator` int(10) unsigned DEFAULT '0' COMMENT '修改人 ID',
   `updated` int(11) NOT NULL COMMENT '修改时间',
-  `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '岗位状态，0-草稿，1-开放，2-关闭',
+  `status` varchar(1) NOT NULL DEFAULT '0' COMMENT '岗位状态，0-草稿，1-开放，2-关闭',
   `finished_date` datetime DEFAULT NULL COMMENT '招聘结束时间',
   `pay_range_from` int(11) unsigned DEFAULT '1' COMMENT '薪资范围（起薪）',
   `pay_range_to` int(11) DEFAULT '1' COMMENT '薪资范围',
-  `job_type` varchar(90) DEFAULT '' COMMENT '职位类型',
+  `job_type` varchar(64) DEFAULT '' COMMENT '职位类型',
   `skills` text COMMENT '技能要求',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='岗位表';
@@ -158,18 +115,18 @@ CREATE TABLE `n2_member` (
   `wx_web` varchar(60) NOT NULL DEFAULT '',
   `company_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属公司 ID',
   `real_name` varchar(90) NOT NULL DEFAULT '' COMMENT '真实姓名',
-  `gender` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '性别，0-保密，1-男，2-女',
+  `gender` varchar(10) NOT NULL DEFAULT '男' COMMENT '性别：保密、男、女',
   `mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '手机号码',
   `email` varchar(60) NOT NULL DEFAULT '' COMMENT 'Email 地址',
-  `addr_province` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '地址：所在省',
-  `addr_city` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '地址：所在市',
-  `addr_area` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '地址：所在地区',
+  `addr_province` varchar(64) NOT NULL DEFAULT '0' COMMENT '地址：所在省',
+  `addr_city` varchar(64) NOT NULL DEFAULT '0' COMMENT '地址：所在市',
+  `addr_area` varchar(64) NOT NULL DEFAULT '0' COMMENT '地址：所在地区',
   `summary` text COMMENT '个人简历',
   `avatar` varchar(512) DEFAULT '' COMMENT '头像地址',
   `nickname` varchar(90) NOT NULL DEFAULT '' COMMENT '微信昵称',
   `profile` varchar(512) DEFAULT NULL COMMENT '一句话简介',
-  `edu_level` int(11) DEFAULT NULL COMMENT '最高学历，0-未知，1-小学，2-初中，3-高中，4-中专，5-大学专科，6-大学本科，7-硕士，8-博士，9-其它',
-  `exp_level` int(11) DEFAULT NULL COMMENT '工作经验，0-应届，1～10-相应年限，99-10 年以上',
+  `edu_level` varchar(32) DEFAULT NULL COMMENT '最高学历：小学、初中、高中、中专、专科、本科、硕士、博士、其它',
+  `exp_level` varchar(32) DEFAULT NULL COMMENT '工作经验：应届、X年、10 年以上',
   PRIMARY KEY (`id`),
   KEY `idx_u_union_id` (`wx_unionid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
@@ -186,8 +143,8 @@ CREATE TABLE `n2_member_company` (
   `member_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属人 ID',
   `company_name` varchar(255) NOT NULL DEFAULT '' COMMENT '公司名称',
   `company_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '关联公司 ID',
-  `work_start_date` smallint(4) unsigned NOT NULL DEFAULT '0' COMMENT '工作开始日期，精确到月份',
-  `work_end_date` smallint(4) NOT NULL DEFAULT '0' COMMENT '工作结束日期，精确到月份',
+  `start_date` date DEFAULT NULL COMMENT '工作开始日期，精确到月份',
+  `end_date` date DEFAULT NULL COMMENT '工作结束日期，精确到月份',
   `created` int(11) NOT NULL COMMENT '创建时间',
   `updated` int(11) NOT NULL COMMENT '修改时间',
   `position_name` varchar(255) NOT NULL DEFAULT '' COMMENT '职位名称',
@@ -209,16 +166,58 @@ CREATE TABLE `n2_member_education` (
   `school_name` varchar(255) NOT NULL DEFAULT '' COMMENT '学校名称',
   `school_id` int(8) unsigned NOT NULL DEFAULT '0' COMMENT '关联标准学校 ID',
   `major_name` varchar(255) NOT NULL DEFAULT '' COMMENT '所学专业名称',
-  `major_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '关联标准专业 ID',
-  `edu_start_date` smallint(4) unsigned NOT NULL DEFAULT '0' COMMENT '学习开始日期，精确到月份',
-  `edu_end_date` smallint(4) unsigned NOT NULL DEFAULT '0' COMMENT '学习结束日期，精确到月份',
-  `edu_level` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '学历（参照个人学历的枚举）',
-  `graduated_year` smallint(4) unsigned NOT NULL DEFAULT '0' COMMENT '毕业年份',
+  `major_id` int(8) unsigned NOT NULL DEFAULT '0' COMMENT '关联标准专业 ID',
+  `start_date` date DEFAULT NULL COMMENT '学习开始日期，精确到月份',
+  `end_date` date DEFAULT NULL COMMENT '学习结束日期，精确到月份',
+  `edu_level` varchar(32) DEFAULT '' COMMENT '学历（参照个人学历的枚举）',
   `created` int(11) NOT NULL COMMENT '创建时间',
   `updated` int(11) NOT NULL COMMENT '修改时间',
-  `summary` varchar(512) DEFAULT NULL COMMENT '描述信息',
+  `summary` varchar(1024) DEFAULT NULL COMMENT '描述信息',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='简历详情（教育经历）';
+
+
+
+# Dump of table n2_resume
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `n2_resume`;
+
+CREATE TABLE `n2_resume` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '简历 ID',
+  `member_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '简历归属人 ID',
+  `created` int(11) NOT NULL COMMENT '创建时间',
+  `updator` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改人 ID',
+  `updated` int(11) NOT NULL COMMENT '修改时间',
+  `summary` text COMMENT '求职意向的补充说明',
+  `post_name` varchar(64) DEFAULT NULL COMMENT '意向职位',
+  `job_type` varchar(32) DEFAULT NULL COMMENT '工作性质（兼职、全职、实习）',
+  `addr_city` varchar(32) DEFAULT NULL COMMENT '意向工作城市',
+  `pay_range_from` int(11) DEFAULT NULL COMMENT '薪水下限（单位：千）',
+  `pay_range_to` int(11) DEFAULT NULL COMMENT '薪水上限（单位：千）',
+  `on_the_job` int(11) DEFAULT NULL COMMENT '是否在职（在职责工作结束日期为：至今）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='简历表';
+
+
+
+# Dump of table n2_resume_delivery
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `n2_resume_delivery`;
+
+CREATE TABLE `n2_resume_delivery` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `resume_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '简历 ID',
+  `member_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '投递人 ID',
+  `delivery_time` datetime NOT NULL COMMENT '投递时间',
+  `delivery_company_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '投递到的公司 ID',
+  `delivery_job_id` int(11) NOT NULL DEFAULT '0' COMMENT '投递',
+  `deliver_status` varchar(32) NOT NULL DEFAULT '0' COMMENT '投递状态，0-待投递，1-已投递',
+  `receiver_status` varchar(32) NOT NULL DEFAULT '0' COMMENT '受理状态，0-待受理，1-已受理',
+  `process_status` varchar(32) NOT NULL DEFAULT '0' COMMENT '处理状态，0-待处理，1-同意，2-拒绝，3-待定',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='简历投递记录';
 
 
 
